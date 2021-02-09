77909365665f195c542d8a60c6e6e2b07405b53f
Liandro Wesley <liandro.silva1012@gmail.com>
2021-02-09 15:02:57 -0300

    feat: add netlify plugins for Lighthouse Audit, sitemap generate and sitemap publish

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,19 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
 
-<style>
+<style lang="scss">
+@import '@/assets/scss/reset.scss';
+@import '@/assets/scss/colors.scss';
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700&display=swap');
+  font-family: 'Open Sans', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: $black;
 }
 
 #nav {

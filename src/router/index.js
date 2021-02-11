b17849bfc910df2714dd44e3722a3f966a228f81
Liandro Wesley <liandro.silva1012@gmail.com>
2021-02-11 20:11:01 -0300

    refactor: refactoring routes

--- src/router/index.js
+++ src/router/index.js
@@ -13,32 +13,44 @@ const routes = [
   {
     path: '/settings',
     name: 'Settings',
-    component: () => import(/* webpackChunkName: "Settings" */ '@/views/view-settings/Index.vue'),
+    component() {
+      return import('@/views/view-settings/Index.vue');
+    },
   },
   {
     path: '/logout',
     name: 'Logout',
-    component: () => import(/* webpackChunkName: "Logout" */ '@/views/view-logout/Index.vue'),
+    component() {
+      return import('@/views/view-logout/Index.vue');
+    },
   },
   {
     path: '/entradas',
     name: 'Entradas',
-    component: () => import(/* webpackChunkName: "Entradas" */ '@/views/view-entradas/Index.vue'),
+    component() {
+      return import('@/views/view-entradas/Index.vue');
+    },
   },
   {
     path: '/saidas',
     name: 'Saidas',
-    component: () => import(/* webpackChunkName: "Saidas" */ '@/views/view-saidas/Index.vue'),
+    component() {
+      return import('@/views/view-saidas/Index.vue');
+    },
   },
   {
     path: '/metas',
     name: 'Metas',
-    component: () => import(/* webpackChunkName: "Metas" */ '@/views/view-metas/Index.vue'),
+    component() {
+      return import('@/views/view-metas/Index.vue');
+    },
   },
   {
     path: '**',
     name: 'PageNotFound',
-    component: () => import(/* webpackChunkName: "PageNotFound" */ '@/views/view-page-not-found/Index.vue'),
+    component() {
+      return import('@/views/view-page-not-found/Index.vue');
+    },
   },
 ];
 

5d5587de7167043ee63b617b54794b0399f8fca1
Liandro Wesley <liandro.silva1012@gmail.com>
2021-02-18 17:25:16 -0300

    refactor: LHCI

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,5 +1,15 @@
 module.exports = {
   ci: {
+    collect: {
+      disableStorageReset: true,
+      // headful: true, // descomente para ver o chrome aberto (util para debug)
+      settings: {
+        chromeFlags: '--no-sandbox',
+      },
+      url: [
+        'https://www.contabilizei.com.br/',
+      ],
+    },
     upload: {
       target: 'temporary-public-storage',
     },
